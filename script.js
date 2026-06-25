5ba8d8ad8cca9002e644f94459c3f012bdd068a7
AgusPe-jeje <agustinbogado03@hotmail.com>
2026-06-25 13:21:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
    ===         🏆 VIRTUAL ALBUM MUNDIAL - ENGINE CORREGIDO 🏆           ===
    ======================================================================== */
 
-const URL_RENDER_SERVICIO = "https://albumpe.onrender.com";
+const URL_RENDER_SERVICIO = "https://prubaalbumpe.onrender.com";
 const URL_BASE = `${URL_RENDER_SERVICIO}/api`;
 
 let usuarioActual = null;
